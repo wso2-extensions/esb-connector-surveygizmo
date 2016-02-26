d5739827ddc535e90d1c66c5310571088c669de9
vives <vives009>
2016-02-26 18:36:53 +0530

    tags added

--- src/test/java/org/wso2/carbon/connector/integration/test/surveygizmo/SurveygizmoConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/surveygizmo/SurveygizmoConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class SurveygizmoConnectorIntegrationTest extends ConnectorIntegrationTes
      */
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("surveygizmo-connector-1.0.0");
+        init("surveygizmo-connector-1.0.1-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
         apiRequestHeadersMap.put("Content-Type", "application/x-www-form-urlencoded");
