a8150afd4a0a917291ffb68dab4be3219e092905
elilmatha <elilsiva3@gmail.com>
2016-10-19 13:02:34 +0530

    Update SurveygizmoConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/surveygizmo/SurveygizmoConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/surveygizmo/SurveygizmoConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class SurveygizmoConnectorIntegrationTest extends ConnectorIntegrationTes
      */
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("surveygizmo-connector-2.0.0");
+        init("surveygizmo-connector-2.0.1-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
         apiRequestHeadersMap.put("Content-Type", "application/json");
